152fc9549b0b5a0844d0969c728d4b15737116c7
Vaibhav <vrongmeal@gmail.com>
2020-02-19 19:46:17 +0530

    Add watcher to pkg.
    Signed-off-by: Vaibhav <vrongmeal@gmail.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/vrongmeal/leaf
 
 go 1.13
+
+require (
+	github.com/fsnotify/fsnotify v1.4.7
+	github.com/prometheus/common v0.9.1
+	github.com/sirupsen/logrus v1.4.2
+	golang.org/x/sys v0.0.0-20200212091648-12a6c2dcc1e4 // indirect
+	gopkg.in/fsnotify.v1 v1.4.7 // indirect
+)
---
+++ pkg/watcher/watcher.go
@@ -0,0 +1,158 @@
+package watcher
+
+import (
+	"fmt"
+	"path/filepath"
+	"sync"
+
+	"github.com/fsnotify/fsnotify"
+	"github.com/sirupsen/logrus"
+
+	"github.com/vrongmeal/leaf/pkg/utils"
+)
+
+// Watcher is the data type that handles watching of paths.
+type Watcher struct {
+	root  string
+	paths []string
+
+	notifier *fsnotify.Watcher
+
+	fileFn FileFunc
+	errFn  ErrorFunc
+	stopFn StopFunc
+
+	done chan bool
+	wg   *sync.WaitGroup
+}
+
+// WatchOpts are the options used to configure the watcher.
+type WatchOpts struct {
+	Root string
+
+	FileFn  FileFunc
+	ErrorFn ErrorFunc
+	StopFn  StopFunc
+}
+
+// FileFunc is the function that runs when a file is received by the channel.
+type FileFunc func(file string)
+
+func nilFileFunc(string) {}
+
+// ErrorFunc is the function that runs when an error occurs while watching.
+type ErrorFunc func(err error)
+
+func nilErrorFunc(error) {}
+
+// StopFunc is the function that runs when the watcher is closed.
+type StopFunc func()
+
+func nilStopFunc() {}
+
+// NewWatcher returns a watcher from the given options.
+func NewWatcher(opts *WatchOpts) (*Watcher, error) {
+	w := &Watcher{}
+
+	isdir, err := utils.IsDir(opts.Root)
+	if err != nil {
+		return nil, err
+	}
+
+	if !isdir {
+		return nil, fmt.Errorf("path '%s' is not a directory", opts.Root)
+	}
+
+	w.root = filepath.Clean(opts.Root)
+	w.paths, err = utils.GetAllDirs(w.root)
+	if err != nil {
+		return nil, err
+	}
+
+	if opts.FileFn == nil {
+		opts.FileFn = nilFileFunc
+	}
+
+	if opts.ErrorFn == nil {
+		opts.ErrorFn = nilErrorFunc
+	}
+
+	if opts.StopFn == nil {
+		opts.StopFn = nilStopFunc
+	}
+
+	w.notifier, err = fsnotify.NewWatcher()
+	if err != nil {
+		return nil, err
+	}
+
+	w.fileFn = opts.FileFn
+	w.errFn = opts.ErrorFn
+	w.stopFn = opts.StopFn
+
+	w.done = make(chan bool)
+	w.wg = &sync.WaitGroup{}
+
+	return w, nil
+}
+
+// Watch executes the watching of files.
+func (w *Watcher) Watch() error {
+	w.wg.Add(1)
+	go func() {
+		defer w.wg.Done()
+		for {
+			select {
+			case event := <-w.notifier.Events:
+				if event.Op == fsnotify.Write {
+					w.handleFile(event.Name)
+				}
+			case err := <-w.notifier.Errors:
+				if err != nil {
+					w.handleErr(err)
+				}
+			case <-w.done:
+				w.handleClose()
+				return
+			}
+		}
+	}()
+
+	for _, f := range w.paths {
+		logrus.Debugln(f)
+		if err := w.notifier.Add(f); err != nil {
+			w.done <- true
+			w.wg.Wait()
+			return err
+		}
+	}
+
+	return nil
+}
+
+// Close terminates the watcher.
+func (w *Watcher) Close() error {
+	w.done <- true
+	w.wg.Wait()
+	return w.notifier.Close()
+}
+
+// Wait waits for the watch to close.
+func (w *Watcher) Wait() {
+	w.wg.Wait()
+}
+
+func (w *Watcher) handleFile(file string) {
+	fileFn := w.fileFn
+	fileFn(file)
+}
+
+func (w *Watcher) handleErr(err error) {
+	errFn := w.errFn
+	errFn(err)
+}
+
+func (w *Watcher) handleClose() {
+	stopFn := w.stopFn
+	stopFn()
+}
